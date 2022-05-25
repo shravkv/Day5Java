f1191f3db2b16bb317d5954c09b1f77ad438282e
sravan <sravan.k.vodnala@gmail.com>
2022-05-25 21:51:07 +0530

    quotient

---
+++ src/quotient/Remainder.java
@@ -0,0 +1,16 @@
+package quotient;
+
+public class Remainder {
+
+    public static void main(String[] args) {
+
+        int dividend = 28, divisor = 5;
+
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+
+        System.out.println("Quotient = " + quotient);
+        System.out.println("Remainder = " + remainder);
+
+    }
+}
